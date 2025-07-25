8064ec71eb4b41688816280c01e053d0544249d3
abhay-109 <abhaypant853@gmail.com>
2025-07-25 20:58:08 +0545

    Create ques4.py

---
+++ lab2/ques4.py
@@ -0,0 +1,15 @@
+stack=[]
+
+stack.append(1)
+print(stack)
+stack.append(4)
+print(stack)
+stack.append(4)
+print(stack)
+
+print(stack)
+# poping the elements
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
